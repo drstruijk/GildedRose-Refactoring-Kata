f5ee938a5693bbbf6f0a0af163c8e8404391b14c
Jeroen Kant <jeroen.kant@momomedical.com>
2023-05-25 16:34:57 +0200

    Seperate brie and backstage logic

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,22 +21,23 @@ export class GildedRose {
     let changeInQuality = 0;
     let changeInSellIn = -1;
 
-    if (
-      item.name == "Aged Brie" ||
-      item.name == "Backstage passes to a TAFKAL80ETC concert"
-    ) {
+    if (item.name == "Aged Brie") {
       if (item.quality < 50) {
         changeInQuality += 1;
-        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              changeInQuality += 1;
-            }
+      }
+    }
+
+    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+      if (item.quality < 50) {
+        changeInQuality += 1;
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            changeInQuality += 1;
           }
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              changeInQuality += 1;
-            }
+        }
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            changeInQuality += 1;
           }
         }
       }
