d10886a824a3f299bc73f3f68b455f0d46130c52
Jeroen Kant <jeroen.kant@momomedical.com>
2023-05-25 16:22:04 +0200

    Fix failing test for conjured quality decrease edge case

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,29 +19,30 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
+      let changeInQuality = 0;
       if (
         this.items[i].name != "Aged Brie" &&
         this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
+            changeInQuality += -1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          changeInQuality += 1;
           if (
             this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
           ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                changeInQuality += 1;
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                changeInQuality += 1;
               }
             }
           }
@@ -50,29 +51,32 @@ export class GildedRose {
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      if (
-        this.items[i].sellIn < 0 ||
-        this.items[i].name.startsWith("Conjured")
-      ) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].name == "Aged Brie") {
+          if (this.items[i].quality < 50) {
+            changeInQuality += 1;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (
+            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+          ) {
+            changeInQuality += -this.items[i].quality;
+          } else {
+            if (this.items[i].quality > 0) {
+              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+                changeInQuality += -1;
+              }
+            }
           }
         }
       }
+
+      if (this.items[i].name.startsWith("Conjured") && changeInQuality < 0) {
+        changeInQuality *= 2;
+      }
+
+      this.items[i].quality += changeInQuality;
     }
 
     return this.items;
